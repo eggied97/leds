50047231500d4a6df4722c7532d27f38d27b2172
Egbert Dijkstra <info@egbert-dijkstra.nl>
2016-03-19 15:10:28 +0100

    https -> http

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ io.on('connection', function(socket) {
 
 
   socket.on('changeColor', function(color){
-      socket.emit("colorChanged", color);
+      io.emit("colorChanged", color);
       pushStringToArduino(color);
   });
 
